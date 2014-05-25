3f7e530cae360bc8b06ca8e50528ac0ce03c62c3
Rafael Rodrigues <rafael.rodrigues@riobravo.com.br>
2014-05-25 11:44:47 -0300

    edited cachematrix.R file, with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+# makeCacheMatrix example
+# type a <- makeCacheMatrix()
+# type a$set(matrix(sample.int(16), 4, 4)). To invert the matrix, we must use a quadratic matrix
+# type a$get() to see the quadratic matrix
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -11,6 +16,10 @@ makeCacheMatrix <- function(x = matrix()) {
              setsolve = setsolve,
              getsolve = getsolve)
 }
+
+# cacheSolve example
+# type cacheSolve(a) to invert the matrix assigned to the "a" variable that we used in the function makeCacheMatrix()
+# type again cacheSolve(a) to invert the matrix. But this time the solve() function will use the matrix stored in memory (m variable). This way is faster than recalculate the invert matrix from scratch.
 
 cacheSolve <- function(x, ...) {
         m <- x$getsolve()
